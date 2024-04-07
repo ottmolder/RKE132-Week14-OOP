3b93f1a2f707f1f68d487accc308d2f428bf4563
Ott Mlder. <ott.molder@tktk.ee>
2024-04-07 20:28:03 +0300

    add week14 code

---
+++ Week14OOP/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.Net.Security;
+
+Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine();
+Dog myDog = new Dog(myDogName);
+
+Dog neightboursDog = new Dog("Good Girl");
+
+
+Console.WriteLine($"My dogs name is {myDog.Name}.");
+Console.WriteLine($"My neighbours dogs name is {neightboursDog.Name}.");
+
+myDog.Rename("Bad Boy");
+
+while (myDog.LevelOfHapiness != 5)
+{
+    myDog.Bark();
+}
+
+myDog.WagTail();
+
+
+class Dog
+{
+    private string _name;
+    private int _levelOfHapiness;
+
+
+    public Dog(string name)
+    {
+        _name = name;
+        _levelOfHapiness = 0;
+    }
+
+    //getter
+    public string Name
+    {
+        get { return _name; }
+    }
+
+
+    public int LevelOfHapiness
+    {
+        get { return _levelOfHapiness; }
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to: {newName}");
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-Woof");
+        _levelOfHapiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-wiggle");
+    }
+
+}
+
